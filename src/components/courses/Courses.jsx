d500b70b0a62459696f2594a27edb130ce734028
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-15 23:47:22 +0200

    add courses card data

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,7 +1,29 @@
-export function Courses() {
+import { CoursesItem } from "./coursesItem/CoursesItem";
+
+export function Courses({courses}) {
     return (
         <div>
-            <h1>Courses section</h1>
+            <h2>Courses section</h2>
+            {courses.map(course => {
+                return (
+                    <CoursesItem 
+                        key={course.id}
+                        id={course.id}
+                        title={course.title}
+                        /* description={} */
+                        containsLockedLessons={course.containsLockedLessons}
+                        duration={course.duration}
+                        launchDate={course.launchDate}
+                        lessonsCount={course.lessonsCount}
+                        previewImageLink={course.previewImageLink}
+                        previewImageAlt={course.meta.slug}
+                        rating={course.rating}
+                        tags={course.tags}
+                        skills={course.meta.skills}
+                        
+                    />
+                );
+            })}
         </div>
     );
 };
--- src/components/courses/coursesItem/CoursesItem.jsx
+++ src/components/courses/coursesItem/CoursesItem.jsx
@@ -1,8 +1,27 @@
-export function CoursesItem ({title, photoSrc, containsLockedLessons, }) {
+export function CoursesItem ({id, title, duration, launchDate, containsLockedLessons, 
+    lessonsCount, previewImageLink, previewImageAlt, rating, tags, skills}) {
     return (
-        <li>
+        <li id={id}>
             <h3>{title}</h3>
             <p>Description: {}</p>
+            {containsLockedLessons && <p>Contains locked lessons</p>}
+            <p>Duration: {duration}minutes</p>
+            <p>Launch date: {launchDate}</p>
+            <p>Lesson count: {lessonsCount}</p>
+            <img src={previewImageLink} alt={previewImageAlt}></img>
+            <p>Raiting: {rating}</p>
+            <ul>Tags: {tags.map(tag => {
+                return (
+                    <li>{tag}</li>
+                );
+            })
+            }</ul>
+            <ul>Skills: {skills/* .map(skill => {
+                return (
+                    <li>{skill}</li>
+                );
+            }) */}</ul>
+
         </li>
     );
 }
