81c6caf88cbc05190a261090427e74133ba93399
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-19 19:24:13 +0200

    add video tag with hls

--- src/components/lessons/currentLesson/CurrentLesson.jsx
+++ src/components/lessons/currentLesson/CurrentLesson.jsx
@@ -1,22 +1,35 @@
-import ReactPlayer from 'react-player';
+//import ReactPlayer from 'react-player';
 import { Div } from './CurrentLesson.styled';
+import Hls from 'hls.js';
 
 export function CurrentLesson ({title, link, number, imgLink}) {
+    const video = document.getElementById('video');
+    const videoSrc = `https://cors-proxy.fringe.zone/${link}`;
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+    }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+    }
  return (
     <Div>
         <h3>Lesson {number} : {title}</h3>
-        <ReactPlayer
+        {/* <ReactPlayer
             url={`https://cors-proxy.fringe.zone/${link}`}
             controls={true}
             width="750px"
             height="auto"
             light={<img src={imgLink} alt='preview' />}
-        />
-        {/* <video
-        src={`https://cors-proxy.fringe.zone/${link}`}
-        poster={`${previewImageLink}/lesson-${number}.webp`}
+        /> */}
+        <video id='video'
+        /* src={`https://cors-proxy.fringe.zone/${link}`} */
+        /* poster={`${previewImageLink}/lesson-${number}.webp`} */
         width="750px"
-        ></video> */}
+        controls
+        ></video>
+        {}
     </Div>
  )
 };
